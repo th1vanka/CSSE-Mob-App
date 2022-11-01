2107c5b5be033dae4512ac508450de8f90560e02
IT20026448 <it20026448@my.sliit.lk>
2022-11-01 12:18:22 +0530

    debicommit

--- csse_mobile_app/lib/order_details.dart
+++ csse_mobile_app/lib/order_details.dart
@@ -1,36 +1,74 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class OrderDetailsPage extends StatelessWidget {
+class OrderDetailsPage extends StatefulWidget{
+  const OrderDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderDetailsPage> createState() => _OrderDetailsPageState();
+}
+class _OrderDetailsPageState extends State<OrderDetailsPage> {
   @override
   Widget build(BuildContext context) {
+    Future openDialog() => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('H-beam (44 mm)'),
+          content: TextField(
+            decoration: InputDecoration(
+            labelText:
+            '''
+             Order qty:20
+             Received qty:0
+             Due date: 20/10/2022
+             Receive Qty
+            ''',
+
+                hintText: 'Enter your name'
+            ),
+          ),
+          actions: [
+            InkWell(
+                onTap: (){},
+                child: Text('SUBMIT'),
+            )
+          ],
+        ),
+    );
     return new Scaffold(
-        body:Column(children: <Widget>[
+        body: Column(children: <Widget>[
           Center(
             child: Text(
-              '''
-              HD2172
-              '''
+            "HD2172",
             ),
           ),
           Row(
             //ROW 1
             children: [
               Container(
-                color: Color.fromRGBO(253, 238, 206, 1),
                 margin: EdgeInsets.all(25.0),
                 width: 300,
-                child: Text(
-                  '''
-                  H-beam
-                  Order qty : 10
-                  Received qty : 0
-                  ''',
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Color.fromRGBO(253, 238, 206, 1),
+                ),
+                child: InkWell(
+                  
+                  child: Text(
+                    '''
+                     H-beam (44mm)
+                     Order qty: 10
+                     Received qty: 0
+                    '''
+                  ),
+                  onTap: () {
+                    openDialog();
+                  },
                 ),
               ),
             ],
           ),
-          Row(//ROW 2
+          Row( //ROW 2
               children: [
                 Container(
                   color: Color.fromRGBO(253, 238, 206, 1),
@@ -45,7 +83,7 @@ class OrderDetailsPage extends StatelessWidget {
                   ),
                 ),
               ]),
-          Row(// ROW 3
+          Row( // ROW 3
               children: [
                 Container(
                   color: Color.fromRGBO(253, 238, 206, 1),
@@ -61,5 +99,8 @@ class OrderDetailsPage extends StatelessWidget {
                 ),
               ]),
         ]));
+    void submit(){
+      Navigator.of(context).pop();
+    }
   }
 }
